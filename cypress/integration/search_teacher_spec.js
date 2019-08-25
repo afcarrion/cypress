c902ba0b546ea9dcb367ea00194fe91a0d14b940
afcarrion <af.carrion@uniandes.edu.co>
2019-08-24 23:06:49 -0500

    Filtro por materias

--- cypress/integration/search_teacher_spec.js
+++ cypress/integration/search_teacher_spec.js
@@ -25,3 +25,28 @@ describe('Prueba ir a la pagina del profesor', function(){
 		cy.get('#react-select-required_error_checksum--option-0').click()
 	})
 })
+
+describe('Filtros por materia', function(){
+	it('Visits Los estudiantes and search for subject', function(){
+		cy.visit('https://losestudiantes.co')
+		cy.contains('Cerrar').click()
+		cy.get('.buscador').click()
+		cy.get('.Select-input  input').type("Rubby")
+		cy.wait(1000)
+		cy.get('.Select-input  input').clear()
+		cy.wait(1000)
+		cy.get('.Select-input  input').type("Mario Linares")
+		cy.get('#react-select-required_error_checksum--option-0').click()
+		cy.wait(1000)
+
+		//Prueba un filtro
+		cy.get('.materias').find('[type="checkbox"]').first().check()
+		cy.wait(600)
+		//Prueba Varios Filtros
+		cy.get('.materias').find('[type="checkbox"]').first().uncheck()
+		cy.wait(600)
+		cy.get('.materias').find('[name="id:MISO4208"]').check()
+		cy.get('.materias').find('[name="id:ISIS1206"]').check()
+
+	})
+})
